56c4ef8fb4fb696b7872eca55fa5d1f6a9e2a33e
gulbaharrkalkann <gulbaharrkalkan@gmail.com>
2025-08-17 10:23:57 +0300

    scheduled kullanımı

--- src/main/java/com/gulkalkan/starter/ExceptionManagementStarter.java
+++ src/main/java/com/gulkalkan/starter/ExceptionManagementStarter.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.gulkalkan"})
 @EnableJpaRepositories(basePackages = {"com.gulkalkan"})
 @ComponentScan(basePackages = {"com.gulkalkan"})
-
+@EnableScheduling
 @SpringBootApplication
 public class ExceptionManagementStarter {
 
